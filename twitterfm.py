56886dc3a040029f4c93e17d6a63512c1bba94c1
ideafin <mz.adam@gmail.com>
2012-10-15 22:44:30 -0500

    added lastfm username search - now displays tweets about top artists for that lastfm user

--- twitterfm.py
+++ twitterfm.py
@@ -1,18 +1,34 @@
 import cgi
 import json
+import pickle
 import urllib
 import urllib2
 import webapp2
 
 from google.appengine.api import users
 
+api_key = pickle.load(open('api_key.pic', 'rb'))
+
 class MainPage(webapp2.RequestHandler):
+
+	def getTopArtists(self, username):
+		base_url = 'http://ws.audioscrobbler.com/2.0/'
+		query = '?method=user.getweeklyartistchart&user=' + urllib.quote(username) + '&api_key=' + api_key + '&format=json'
+		lastfmResponse = urllib2.urlopen(base_url + query)
+		return json.loads(lastfmResponse.read())
 
 	def searchTwitter(self, searchString):
 		base_url = 'http://search.twitter.com/search.json?q='
-		query = urllib.quote(searchString)
-		twitterResponse = urllib2.urlopen(base_url + query)
-		return json.loads(twitterResponse.read())
+		twitterResponse = None
+		try:
+			query = urllib.quote(searchString)
+			twitterResponse = urllib2.urlopen(base_url + query)
+		except KeyError:
+			pass
+		except urllib2.HTTPError:
+			pass
+		if twitterResponse != None:
+			return json.loads(twitterResponse.read())
 
 	def displayTweet(self, tweet):
 		self.response.write('<p>')
@@ -33,9 +49,13 @@ class MainPage(webapp2.RequestHandler):
 			</html>""")
 
 	def post(self):
-		data = self.searchTwitter(self.request.get('content'))
-		
-		for result in data['results']:
-			self.displayTweet(result)
+		artists = self.getTopArtists(self.request.get('content'))
 
+		for artist in artists['weeklyartistchart']['artist']:
+			self.response.write(artist['name'])
+			tweets = self.searchTwitter(artist['name'])
+			if tweets != None:
+				for tweet in tweets['results']:
+					self.displayTweet(tweet)
+	
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
